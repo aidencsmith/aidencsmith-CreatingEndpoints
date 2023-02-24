cc5cea2e720b0fee4c307cb58c1a028a40c96506
aidencsmith <acsmith7643@gmail.com>
2023-02-24 21:13:29 +0000

    endpoint commit

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -16,7 +16,11 @@ public class JavalinSingleton {
      */
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
-        
+
+        app.get("/hello", ctx -> {
+            //logic to be executed when this endpoint is hit
+            ctx.result("Hello World");
+        });
         //write endpoint here
 
         return app;
